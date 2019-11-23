d83508d4aa7122873159d9bf6c991bef86de7722
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-11-23 11:23:35 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pkv',
+      version='1.0',
+      description='A minimalistic persistent key value store.',
+      author='Jayjeet Chakraborty',
+      author_email='jayjeetchakraborty25@gmail.com',
+      license='MIT',
+      zip_safe=False)
