4fa20f3018131f75b17f5cbb669bf900509f7a60
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-11-23 12:19:54 +0530

    Bump up version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pkv',
-      version='1.0.1',
+      version='1.0.2',
       description='A minimalistic persistent key value store.',
       author='Jayjeet Chakraborty',
       author_email='jayjeetchakraborty25@gmail.com',
