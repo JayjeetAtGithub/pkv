07cc30322bcc679c09da704b2ff52e157c610b8f
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-11-24 12:11:54 +0530

    Add syntactic sugars

--- pkv/main.py
+++ pkv/main.py
@@ -6,14 +6,56 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
+
 __version__ = 1.0
 
+
 class PKV(object):
+    """A persistent key-value database.
+    It stores the data in `yaml` format.
+
+    Example:
+        >>> from pkv import PKV
+        >>> db = PKV('/path/to/mydatabase.db')
+        >>> db.set('key1', 'value1')
+        >>> db.get('key1')
+        >>> db.erase('key1')
+    """
 
     def __init__(self, db='pkv.db'):
         self._db = db
         self._lock = threading.Lock()
         self._init_db()
+
+    def __getitem__(self, key):
+        """Getter method to fetch from the
+        DB like a dict.
+
+        Example:
+            >>> db = PKV()
+            >>> print(db['name'])
+        """
+        return self.get(key)
+
+    def __setitem__(self, key, value):
+        """Setter method to insert key, value
+        pairs in the DB like a dict.
+
+        Example:
+            >>> db = PKV()
+            >>> db['name'] = 'John'
+        """
+        return self.set(key, value)
+
+    def __delitem__(self, key):
+        """Remove key, value pairse from the DB
+        using the `del` keyword.
+
+        Example:
+            >>> db = PKV()
+            >>> del db['name']
+        """
+        return self.erase(key)
 
     def _init_db(self):
         if not os.path.exists(self._db):
